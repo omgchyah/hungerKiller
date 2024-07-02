a78d57e1584911ec8ca09755b67e47f47b89f7d6
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-02 11:40:10 +0200

    Create recipe complete

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -3,5 +3,13 @@
     <h1>Aquí se mostrará!</h1>
     <div>Nombre: {{$recipe->name}}</div>
     <div>Descripción: {{ $recipe->description }}</div>
+    <div>Servings: {{ $recipe->servings }}</div>
+    <div>Difficulty: {{ $recipe->difficulty }}</div>
+    <div>Category: {{ $recipe->category }}</div>
+    <div>Restrictions: {{ $recipe->restrictions }}</div>
+    <div>Prep time: {{ $recipe->restrictions }}</div>
+    <div>Cooking time: {{ $recipe->restrictions }}</div>
+    <div>Restrictions: {{ $recipe->restrictions }}</div>
     <div>Instrucciones: {{ $recipe->instructions }}</div>
+    <div></div>
 </x-main-layout>
